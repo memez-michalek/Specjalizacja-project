61ff1dff499820c43b3af79cb9f30df0de045be8
memez-michalek <michal.lesniowski1@protonmail.com>
2022-10-17 22:22:34 +0200

    started working on frontend

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,21 @@
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import NavDropdown from "react-bootstrap/NavDropdown"
+import Component from "react"
+
+class Navigation extends Component {
+    render() {
+        return (
+        <Navbar>
+            <Coontainer>
+                <Navbar.Brand>
+                    Your-Trap-Lore
+                </Navbar.Brand>
+            </Coontainer>
+        </Navbar>
+        )
+    }
+}
+
+export default Navigation;
