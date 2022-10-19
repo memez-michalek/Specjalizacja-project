d2b506024863ad26d6d3e2fc17437fb8f8725327
memez-michalek <michal.lesniowski1@protonmail.com>
2022-10-19 23:26:37 +0200

    minor additions such as functional router and login/register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 //import {BrowserRouter as Router , Route} from "react-router-dom";
 import Navigation from './components/navbar'
 import Login from './pages/login'
+import {Routes, Route} from "react-router-dom";
+
 function App() {
   return (
-      <Login></Login>
-
+     <Routes>
+        {/*<Route path="/" element={<Home/>}></Route>*/}
+        <Route path="/login" element={<Login/>}></Route>
+     </Routes>
   );
 }
 
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,6 +3,7 @@ import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import NavDropdown from "react-bootstrap/NavDropdown"
 import Component from "react"
+import {Link} from "react-router-dom"
 
 function Navigation() {
         return (
@@ -12,8 +13,8 @@ function Navigation() {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
+                  <Nav.Link><Link to="/login">Login</Link></Nav.Link>
+                  <Nav.Link><Link to="/register">Register</Link>Register</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
             </Container>
